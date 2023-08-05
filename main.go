418a61d5cd05d86a00a3aaa514dc2195f8d76916
mrn3088 <r7ma3088@gmail.com>
2023-08-06 01:04:16 +0800

    feat:main client program updated main client program

--- main.go
+++ main.go
@@ -6,25 +6,22 @@ import (
 )
 
 func main() {
-	// engine
 	r := zee.New()
-
-	// add handlers
 	r.GET("/", func(c *zee.Context) {
-		c.HTML(http.StatusOK, "<h1>Hello Zee</h1>")
+		c.HTML(http.StatusOK, "<h1>Hello zee</h1>")
 	})
 
 	r.GET("/hello", func(c *zee.Context) {
 		c.String(http.StatusOK, "hello %s, you're at %s\n", c.Query("name"), c.Path)
 	})
 
-	r.POST("/login", func(c *zee.Context) {
-		c.JSON(http.StatusOK, zee.H{
-			"username": c.PostForm("username"),
-			"password": c.PostForm("password"),
-		})
+	r.GET("/hello/:name", func(c *zee.Context) {
+		c.String(http.StatusOK, "hello %s, you're at %s\n", c.Param("name"), c.Path)
+	})
+
+	r.GET("/assets/*filepath", func(c *zee.Context) {
+		c.JSON(http.StatusOK, zee.H{"filepath": c.Param("filepath")})
 	})
 
 	r.Run(":9999")
-
 }
