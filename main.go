fb8dc84711a79b5454817b1c00acf4733a49546f
mrn3088 <r7ma3088@gmail.com>
2023-08-06 19:09:32 +0800

    feat:group control added group control, updated client program

--- main.go
+++ main.go
@@ -7,21 +7,33 @@ import (
 
 func main() {
 	r := zee.New()
+
 	r.GET("/", func(c *zee.Context) {
 		c.HTML(http.StatusOK, "<h1>Hello zee</h1>")
 	})
 
-	r.GET("/hello", func(c *zee.Context) {
-		c.String(http.StatusOK, "hello %s, you're at %s\n", c.Query("name"), c.Path)
-	})
+	v1 := r.Group("/v1")
+	{
+		v1.GET("/", func(c *zee.Context) {
+			c.HTML(http.StatusOK, "<h1>Hello Gee</h1>")
+		})
+		v1.GET("/hello", func(c *zee.Context) {
+			c.String(http.StatusOK, "hello %s, you're at %s\n", c.Query("name"), c.Path)
+		})
+	}
 
-	r.GET("/hello/:name", func(c *zee.Context) {
-		c.String(http.StatusOK, "hello %s, you're at %s\n", c.Param("name"), c.Path)
-	})
-
-	r.GET("/assets/*filepath", func(c *zee.Context) {
-		c.JSON(http.StatusOK, zee.H{"filepath": c.Param("filepath")})
-	})
+	v2 := r.Group("/v2")
+	{
+		v2.GET("/hello/:name", func(c *zee.Context) {
+			c.String(http.StatusOK, "hello %s, you're at %s\n", c.Param("name"), c.Path)
+		})
+		v2.POST("/login", func(c *zee.Context) {
+			c.JSON(http.StatusOK, zee.H{
+				"username": c.PostForm("username"),
+				"password": c.PostForm("password"),
+			})
+		})
+	}
 
 	r.Run(":9999")
 }
